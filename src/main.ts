f0e89a5b3930b823c2b9ed2cc5a9e2fd34bcbad6
Ayman Alhaik <AymanAlhaikg@Gmail.com>
2025-11-11 03:20:41 +0300

    adding CreateUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,       // strips properties not in the DTO
+      forbidNonWhitelisted: true, // throws error if unknown properties are sent
+      transform: true,       // automatically transforms payloads to DTO instances
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,60 @@
-export class CreateUserDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl, Max,
+  MaxLength, Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3, { message: 'Name must be at least 3 characters long' })
+  @MaxLength(30, { message: 'Name must be at most 30 characters long' })
+  name: string;
+
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  email: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Password must be at least 3 characters long' })
+  password: string;
+
+  @IsEnum(['user', 'admin'], { message: 'Role must be either user or admin' })
+  role: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUrl({}, {message:'avatar must be a valid url'})
+  avatar?: string;
+
+  @IsOptional()
+  @IsNumber({}, {message:'age must be a number' })
+  @Min(18, { message: 'Age must be at least 18' })
+  age?: number;
+
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  @IsEnum([true, false], {message: 'active must be either true or false'})
+  active?: boolean; // ✅ better as boolean, not string
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6, { message: 'verificationCode must be at least 6 characters long' })
+  verificationCode?: string;
+
+  @IsEnum(['male', 'female'], { message: 'Gender must be either male or female' })
+  gender: string;
+
+}
